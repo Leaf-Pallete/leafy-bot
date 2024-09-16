289121682dee7d1b1474e9ff580c301804af05e2
salviotonon <salvio.tonon@gmail.com>
2024-09-16 11:10:22 -0300

    feat: deploy leafy bot

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import { Hono } from 'hono';
+import { Button, Components, DiscordHono } from 'discord-hono';
 
-const app = new Hono<{ Bindings: CloudflareBindings }>();
-
-app.get('/', (c) => {
-  return c.text('Hello Hono! ~~~~');
-});
+const app = new DiscordHono()
+  .command('hello', (c) => c.res('world!'))
+  .command('help', (c) =>
+    c.res({
+      content: `text: ${c.var.text}`,
+      components: new Components().row(
+        new Button('https://discord-hono.luis.fun', 'Docs', 'Link'),
+        new Button('delete-self', 'Delete', 'Secondary').emoji({ name: '🗑️' }),
+      ),
+    }),
+  )
+  .component('delete-self', (c) => c.resDeferUpdate(c.followupDelete));
 
 export default app;
